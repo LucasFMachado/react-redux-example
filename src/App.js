29ddde1f50bb9e86b7e52a7452b0c7032b602b3b
Lucas Machado <lucas.fmachado19@gmail.com>
2020-05-03 22:58:08 -0300

    Criação das rotas e dos estilos globais da pagina

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+import Router from './routes';
 
 function App() {
-  return <h1>Hello Redux</h1>;
+  return (
+    <BrowserRouter>
+      <Router />
+      <GlobalStyle />
+    </BrowserRouter>
+  );
 }
 
 export default App;
